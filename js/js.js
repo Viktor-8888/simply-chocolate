daf55a404da307e33416294b46acdf27da39f5af
Viktor_Shcherbyna <ahmatov@ukr.net>
2025-08-22 15:20:37 +0300

    added js and footer and modal

---
+++ js/js.js
@@ -0,0 +1,23 @@
+const modal = document.querySelector('.js-modal');
+const modalBtnOpen = document.querySelector('.js-open-modal');
+const modalBtnClose = document.querySelector('.js-close-modal');
+const menu = document.querySelector('.js-menu');
+const menuBtnOpen = document.querySelector('.js-open-menu');
+const menuBtnClose = document.querySelector('.js-close-menu');
+const menuLinkHome = document.querySelector('.js-link-menu-home');
+const menuLinkMade = document.querySelector('.js-link-menu-made');
+const menuLinkReviews = document.querySelector('.js-link-menu-reviews');
+
+modalBtnOpen.addEventListener('click', toggleModal);
+modalBtnClose.addEventListener('click', toggleModal);
+function toggleModal() {
+  modal.classList.toggle('is-open');
+}
+menuBtnOpen.addEventListener('click', toggleMenu);
+menuBtnClose.addEventListener('click', toggleMenu);
+menuLinkHome.addEventListener('click', toggleMenu);
+menuLinkMade.addEventListener('click', toggleMenu);
+menuLinkReviews.addEventListener('click', toggleMenu);
+function toggleMenu() {
+  menu.classList.toggle('is-open');
+}
